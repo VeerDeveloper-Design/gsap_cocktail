13ac423d62d5debf1c83d8b877aab23ed8058b28
Vera Best <vera@mac.home>
2025-06-26 08:34:57 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {ScrollTrigger, SplitText} from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <div className='flex-center v-[100vh]'>
+      
+    </div>
+  )
+}
+
+export default App
